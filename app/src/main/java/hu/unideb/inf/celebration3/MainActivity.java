bc55b2d4516dcd6d2e281e88bad981ca55974ec4
Kocsis Gergely <kocsisger@gmail.com>
2026-04-21 13:35:24 +0200

    UI commit2

--- app/src/main/java/hu/unideb/inf/celebration3/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/celebration3/MainActivity.java
@@ -8,17 +8,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import hu.unideb.inf.celebration3.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+        binding.downloadButton.setOnClickListener(view -> download());
+    }
+
+    private void download() {
+
     }
 }
